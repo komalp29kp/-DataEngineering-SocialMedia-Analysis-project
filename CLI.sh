8add69a39644a9a5d20c50141979ee2f326744eb
Komal Pawar <123524689+komalp29kp@users.noreply.github.com>
2023-05-30 22:09:20 -0500

    CLI.sh

---
+++ CLI.sh
@@ -0,0 +1,13 @@
+# Copy JSON Reference data to same location
+aws s3 cp . s3://de-on-youtubedata-raw-useast-1-dev/youtube/raw_statistics_reference_data/ --recursive --exclude "*" --include "*.json"
+#  copying all data files into its location in Hive-style patterns:
+aws s3 cp CAvideos.csv s3://de-on-youtubedata-raw-useast-1-dev/youtube/raw_statistics/region=ca/
+aws s3 cp DEvideos.csv s3://de-on-youtubedata-raw-useast-1-dev/youtube/raw_statistics/region=de/
+aws s3 cp FRvideos.csv s3://de-on-youtubedata-raw-useast-1-dev/youtube/raw_statistics/region=fr/
+aws s3 cp GBvideos.csv s3://dde-on-youtubedata-raw-useast-1-dev/youtube/raw_statistics/region=gb/
+aws s3 cp INvideos.csv s3://de-on-youtubedata-raw-useast-1-dev/youtube/raw_statistics/region=in/
+aws s3 cp JPvideos.csv s3://de-on-youtubedata-raw-useast-1-dev/youtube/raw_statistics/region=jp/
+aws s3 cp KRvideos.csv s3://de-on-youtubedata-raw-useast-1-dev/youtube/raw_statistics/region=kr/
+aws s3 cp MXvideos.csv s3://dde-on-youtubedata-raw-useast-1-dev/youtube/raw_statistics/region=mx/
+aws s3 cp RUvideos.csv s3://dde-on-youtubedata-raw-useast-1-dev/youtube/raw_statistics/region=ru/
+aws s3 cp USvideos.csv s3://de-on-youtubedata-raw-useast-1-dev/youtube/raw_statistics/region=us/
