657836403424ec1a8a56bb344070fc63176a61b8
Komal Pawar <123524689+komalp29kp@users.noreply.github.com>
2023-05-30 22:06:27 -0500

    Pysparkcode.py

--- Pysparkcode.py
+++ Pysparkcode.py
@@ -1,33 +1,43 @@
 import sys
+import os
 from awsglue.transforms import *
 from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
-from awsglue.dynamicframe import DynamicFrame
 
-## @params: [JOB_NAME]
-arguments = getResolvedOptions(sys.argv, ['JOB_NAME'])
+args = getResolvedOptions(sys.argv, ['JOB_NAME'])
 
-spark_context = SparkContext()
-glue_context = GlueContext(spark_context)
-spark_session = glue_context.spark_session
-glue_job = Job(glue_context)
-glue_job.init(arguments['JOB_NAME'], arguments)
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args['JOB_NAME'])
 
-region_filter = "region in ('ca','gb','us')"
+bucket_name = "de-on-youtubedata-cleaned-athenajob"
 
-source_dynamic_frame = glue_context.create_dynamic_frame.from_catalog(database="db_youtube_raw", table_name="raw_statistics", transformation_ctx="source_dynamic_frame", push_down_predicate=region_filter)
+# Copy JSON Reference data to the specified location
+json_reference_location = "s3://{}/youtube/raw_statistics_reference_data/".format(bucket_name)
+json_reference_command = "aws s3 cp . {} --recursive --exclude '*' --include '*.json'".format(json_reference_location)
+os.system(json_reference_command)
 
-apply_mapping_frame = ApplyMapping.apply(frame=source_dynamic_frame, mappings=[("video_id", "string", "video_id", "string"), ("trending_date", "string", "trending_date", "string"), ("title", "string", "title", "string"), ("channel_title", "string", "channel_title", "string"), ("category_id", "long", "category_id", "long"), ("publish_time", "string", "publish_time", "string"), ("tags", "string", "tags", "string"), ("views", "long", "views", "long"), ("likes", "long", "likes", "long"), ("dislikes", "long", "dislikes", "long"), ("comment_count", "long", "comment_count", "long"), ("thumbnail_link", "string", "thumbnail_link", "string"), ("comments_disabled", "boolean", "comments_disabled", "boolean"), ("ratings_disabled", "boolean", "ratings_disabled", "boolean"), ("video_error_or_removed", "boolean", "video_error_or_removed", "boolean"), ("description", "string", "description", "string"), ("region", "string", "region", "string")], transformation_ctx="apply_mapping_frame")
+# Copy data files to their respective locations
+data_files = {
+    "CAvideos.csv": "ca",
+    "DEvideos.csv": "de",
+    "FRvideos.csv": "fr",
+    "GBvideos.csv": "gb",
+    "INvideos.csv": "in",
+    "JPvideos.csv": "jp",
+    "KRvideos.csv": "kr",
+    "MXvideos.csv": "mx",
+    "RUvideos.csv": "ru",
+    "USvideos.csv": "us"
+}
 
-resolve_choice_frame = ResolveChoice.apply(frame=apply_mapping_frame, choice="make_struct", transformation_ctx="resolve_choice_frame")
+for file_name, region in data_files.items():
+    file_location = "s3://{}/youtube/raw_statistics/region={}/{}".format(bucket_name, region, file_name)
+    file_command = "aws s3 cp {} {}".format(file_name, file_location)
+    os.system(file_command)
 
-drop_null_fields_frame = DropNullFields.apply(frame=resolve_choice_frame, transformation_ctx="drop_null_fields_frame")
-
-sink_data_frame = drop_null_fields_frame.toDF().coalesce(1)
-dynamic_frame_final_output = DynamicFrame.fromDF(sink_data_frame, glue_context, "dynamic_frame_final_output")
-sink_options = {"path": "s3://de-on-youtube-cleansed-useast1-dev/youtube/raw_statistics/", "partitionKeys": ["region"]}
-sink_data_frame = glue_context.write_dynamic_frame.from_options(frame=dynamic_frame_final_output, connection_type="s3", connection_options=sink_options, format="parquet", transformation_ctx="sink_data_frame")
-
-glue_job.commit()
+job.commit()
